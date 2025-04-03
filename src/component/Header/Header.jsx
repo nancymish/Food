18f67de644b914ae7f1741fc3ee8d8b3262f8406
nancymish <143929737+nancymish@users.noreply.github.com>
2025-04-03 17:44:45 +0530

    Change Header

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import './Header.css' 
+import React from "react";
+import "./Header.css";
+import { assets } from "../../assets/assets";
 
 const Header = () => {
   return (
-    <div className='header'>
+    <div
+      className="header"
+      style={{ backgroundImage: `url(${assets.header_img1})` }}
+    >
       <div className="header-content">
         <h1>order your favourite food here</h1>
-        <p>Integrate the food ordering system in your restaurant and grab the future. our restaurant online ordering software is capable enough to serve your purposes successfully.</p>
+        <p>
+          Integrate the food ordering system in your restaurant and grab the
+          future. our restaurant online ordering software is capable enough to
+          serve your purposes successfully.
+        </p>
         <button>View Menu</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
